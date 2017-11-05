199848e4d20075b05cce932c3589de7ffa82a3e3
FluffyHeinzelman <micha.meyer@im-c.de>
2017-11-05 09:25:25 +0100

    Bump version to 1.0.3

--- src/Module.php
+++ src/Module.php
@@ -9,7 +9,7 @@ use Zend\ModuleManager\Feature;
 
 class Module implements Feature\ConfigProviderInterface
 {
-    const VERSION = '1.0.3-dev';
+    const VERSION = '1.0.3';
 
     public function getConfig()
     {
